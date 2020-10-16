4a2b2594bd2e440bd83f6690f7d421c5365669d9
Sergei Nazarenko <nazares@icloud.com>
2020-10-17 02:17:42 +0300

    Publish deploy 2020-10-17 02:17

--- disqus.js
+++ disqus.js
@@ -1,5 +1,13 @@
-(function() {
-    var t = document,
-        e = t.createElement("script");
-    e.src = "https://nazares.disqus.com/embed.js", e.setAttribute("data-timestamp", +new Date), (t.head || t.body).appendChild(e)
-})();
+window.talktype = window.talktype || function(f) {
+    if (talktype.loaded)
+      f();
+    else
+      (talktype.q = talktype.q || []).push(arguments);
+  };
+talktype(function() {
+    var pageUrl = window.location.href;
+    var pageMetadata = < metadata - object - here > ;
+    var accountId = '1000699';
+    var metypeHost = 'https://www.metype.com/';
+    talktype.pageMetadataSetter(accountId, pageUrl, pageMetadata, metypeHost);
+  });
