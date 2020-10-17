34572ae0232dbb2838d7ac0b1d445d7b3ea17ca5
Sergei Nazarenko <nazares@icloud.com>
2020-10-17 17:31:52 +0300

    Publish deploy 2020-10-17 17:31

--- wpcomment.js
+++ wpcomment.js
@@ -1,10 +1,28 @@
-AnyComment = window.AnyComment || []; AnyComment.Comments = [];
-AnyComment.Comments.push({
-    "root": "anycomment-app",
+//AnyComment = window.AnyComment || []; AnyComment.Comments = [];
+//AnyComment.Comments.push({
+//    "root": "anycomment-app",
     "app_id": 1524,
-    "language": "ru"
-})
-var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-s.src = "https://widget.anycomment.io/comment/embed.js";
-var sa = document.getElementsByTagName("script")[0];
-sa.parentNode.insertBefore(s, s.nextSibling);
+//    "language": "ru"
+//})
+//var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+//s.src = "https://widget.anycomment.io/comment/embed.js";
+//var sa = document.getElementsByTagName("script")[0];
+//sa.parentNode.insertBefore(s, s.nextSibling);
+
+
+AnyComment = window.AnyComment || []; AnyComment.Login = [];
+    window.AnyComment.Login.push({
+        root: 'anycomment-login',
+        app_id: 1524,
+        providers: 'vkontakte,twitter,odnoklassniki,google,mailru',
+        options: {
+            show_login_with: false,
+        },
+        onLogin: function (envelope) {
+            console.log(envelope);
+        }
+    });
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://cdn.anycomment.io/assets/js/login.js";
+    var sa = document.getElementsByTagName("script")[0];
+    sa.parentNode.insertBefore(s, s.nextSibling);
