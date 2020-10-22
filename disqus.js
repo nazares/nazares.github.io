029bf28a8bf09f4f370675dffa654b46d9282dc3
Sergei Nazarenko <nazares@icloud.com>
2020-10-22 19:24:46 +0300

    Publish deploy 2020-10-22 19:24

--- disqus.js
+++
@@ -1,5 +0,0 @@
-const commentBox = require('commentbox.io');
-// or if using the CDN, it will be available as a global "commentBox" variable.
-
-commentBox('5688337281056768-proj');
-
--- disqus_1.js
+++
@@ -1,4 +0,0 @@
-const commentBox = require('commentbox.io');
-// or if using the CDN, it will be available as a global "commentBox" variable.
-
-commentBox('my-project-id');
