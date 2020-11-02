daf7fcdf311748becb3d9d013044a3847dfd7ada
Sergei Nazarenko <nazares@icloud.com>
2020-11-02 13:38:21 +0300

    Publish deploy 2020-11-02 13:38

--- Blog/anycomment.js
+++ Blog/anycomment.js
@@ -2,7 +2,7 @@ AnyComment = window.AnyComment || []; AnyComment.Comments = [];
 AnyComment.Comments.push({
     "root": "anycomment-app",
     "app_id": 1524,
-    "language": "en"
+    "language": "ru"
 })
 var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
 s.src = "https://widget.anycomment.io/comment/embed.js";
